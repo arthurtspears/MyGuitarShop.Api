1e2530a3e6dc41fdb9f44f42ae34287d72ca1523
Arthur Spears <arthurtspears@gmail.com>
2025-10-24 16:00:57 -0700

    Added db health and health checks. Added Products controller with ProductsRepo Implemented async/await patterning.

---
+++ MyGuitarShop.Data.Ado/Repositories/ProductRepo.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using MyGuitarShop.Data.Ado.Entities;
+using MyGuitarShop.Data.Ado.Factories;
+
+namespace MyGuitarShop.Data.Ado.Repositories
+{
+    public class ProductRepo(
+        ILogger<ProductRepo> logger, 
+        SqlConnectionFactory connectionFactory)
+    {
+        public async Task<IEnumerable<ProductEntity>> GetAllProductsAsync()
+        {
+            var products = new List<ProductEntity>();
+            
+            try
+            {
+                await using var conn = await connectionFactory.OpenSqlConnectionAsync();
+
+                await using var cmd = new SqlCommand("SELECT * FROM Products", conn);
+
+                await using var reader = await cmd.ExecuteReaderAsync();
+
+                while (await reader.ReadAsync())
+                {
+                    var product = new ProductEntity
+                    {
+                        ProductID = reader.GetInt32(reader.GetOrdinal("ProductID")),
+                        CategoryID = reader.GetInt32(reader.GetOrdinal("CategoryID")),
+                        ProductCode = reader.GetString(reader.GetOrdinal("ProductCode")),
+                        ProductName = reader.GetString(reader.GetOrdinal("ProductName")),
+                        Description = reader.GetString(reader.GetOrdinal("Description")),
+                        ListPrice = reader.GetDecimal(reader.GetOrdinal("ListPrice")),
+                        DiscountPercent = reader.GetDecimal(reader.GetOrdinal("DiscountPercent")),
+                        DateAdded = reader.GetDateTime(reader.GetOrdinal("DateAdded"))
+                    };
+                    products.Add(product);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex.Message, "Error retrieving product list");
+            }
+            return products;
+        }
+    }
+}
