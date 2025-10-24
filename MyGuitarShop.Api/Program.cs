5ee4502353f44a7726c7c4c8332f9ab5a09c9a90
Arthur Spears <arthurtspears@gmail.com>
2025-10-24 14:09:19 -0700

    Db Health Check Connection Successful. Added SqlConnectionFactory, imported Microsoft.Data.SqlClient package Removed WeatherForecast controller/class Added logging and configuration.

--- MyGuitarShop.Api/Program.cs
+++ MyGuitarShop.Api/Program.cs
@@ -1,36 +1,88 @@
 
+using Microsoft.Extensions.DependencyInjection;
+using MyGuitarShop.Data.Ado.Factories;
+using System.Diagnostics;
+using Microsoft.AspNetCore.HttpLogging;
+
 namespace MyGuitarShop.Api
 {
-    public class Program
+    public static class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+                // Add services to the container.
+                AddLogging(builder);
+
+                AddServices(builder);
+
+                if (builder.Environment.IsDevelopment())
+                {
+                    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+                    builder.Services.AddEndpointsApiExplorer();
+                    builder.Services.AddSwaggerGen();
+                }
+
+                var app = builder.Build();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    app.MapGet("/", () => "API running in Development");
+                    app.UseSwagger();
+                    app.UseSwaggerUI();
+                }
+
+                ConfigureApplication(app);
+
+                await app.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                if (Debugger.IsAttached) Debugger.Break();
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void AddLogging(WebApplicationBuilder builder)
+        {
+            builder.Services.AddLogging(logging =>
+            {
+                logging.ClearProviders();
+                logging
+                    .AddFilter("Microsoft", LogLevel.Information)
+                    .AddFilter("Microsoft.AspNetCore.HttpLogging", LogLevel.Information)
+                    .AddConsole();
+            });
+
+            builder.Services.AddHttpLogging(options =>
+            {
+                options.LoggingFields = HttpLoggingFields.RequestPath
+                                        | HttpLoggingFields.RequestMethod
+                                        | HttpLoggingFields.ResponseStatusCode;
+            });
+        }
+
+        private static void AddServices(WebApplicationBuilder builder)
+        {
+            var connectionString = builder.Configuration.GetConnectionString("MyGuitarShop") 
+                ?? throw new InvalidOperationException("MyGuitarShop connection string not found.");
+
+            builder.Services.AddSingleton(new SqlConnectionFactory(connectionString));
+
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+        }
 
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            app.UseHttpsRedirection();
-
+        private static void ConfigureApplication(WebApplication app)
+        {
+            app.UseHttpLogging();
             app.UseAuthorization();
-
-
             app.MapControllers();
-
-            app.Run();
         }
     }
 }
