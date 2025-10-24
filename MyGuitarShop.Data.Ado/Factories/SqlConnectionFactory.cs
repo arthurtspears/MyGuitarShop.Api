3378d595dacfbad52a75b9e825542c5ce76d05d1
Arthur Spears <arthurtspears@gmail.com>
2025-10-24 14:09:19 -0700

    Db Health Check Connection Successful. Added SqlConnectionFactory, imported Microsoft.Data.SqlClient package Removed WeatherForecast controller/class Added logging and configuration.

---
+++ MyGuitarShop.Data.Ado/Factories/SqlConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿
+
+using Microsoft.Data.SqlClient;
+
+namespace MyGuitarShop.Data.Ado.Factories
+{
+    public class SqlConnectionFactory(string connectionString)
+    {
+        public SqlConnection OpenSqlConnection()
+        {
+            var connection = new SqlConnection(connectionString);
+            connection.Open();
+            return connection;
+        }
+    }
+}
