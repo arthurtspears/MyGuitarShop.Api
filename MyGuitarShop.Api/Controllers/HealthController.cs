775f7670c334f5ad5622fb8f6f40c288b8993721
Arthur Spears <arthurtspears@gmail.com>
2025-10-24 14:09:19 -0700

    Db Health Check Connection Successful. Added SqlConnectionFactory, imported Microsoft.Data.SqlClient package Removed WeatherForecast controller/class Added logging and configuration.

---
+++ MyGuitarShop.Api/Controllers/HealthController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyGuitarShop.Data.Ado.Factories;
+
+namespace MyGuitarShop.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController(
+        ILogger<HealthController> logger,
+        SqlConnectionFactory sqlConnectionFactory) 
+        : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok("Healthy");
+            }
+            catch (Exception)
+            {
+                logger.LogWarning("Health check failed unreasonably.");
+
+                return StatusCode(503, "Unhealthy");
+            }
+        }
+
+        [HttpGet("db")]
+        public IActionResult GetDbHealth()
+        {
+            try
+            {
+                using var connection = sqlConnectionFactory.OpenSqlConnection();
+                return Ok(new { Message = "Connection successful!", connection.Database });
+            }
+            catch (Exception)
+            {
+                logger.LogCritical("Database health check failed.");
+
+                return StatusCode(503, "Database Unhealthy");
+            }
+        }
+    }
+}
