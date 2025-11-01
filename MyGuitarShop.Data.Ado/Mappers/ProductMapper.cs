7c134aea3bc42f1e6b9b36249a7a563600f8fd70
Arthur Spears <arthurtspears@gmail.com>
2025-11-01 12:37:04 -0700

    Adding ProductDto.cs, ProductMapper.cs, and CategoriesEnum.cs. Added additional endpoints and repositories for Products.

---
+++ MyGuitarShop.Data.Ado/Mappers/ProductMapper.cs
@@ -0,0 +1,41 @@
+﻿using MyGuitarShop.Common.Dtos;
+using MyGuitarShop.Data.Ado.Entities;
+
+namespace MyGuitarShop.Data.Ado.Mappers
+{
+    public static class ProductMapper
+    {
+        public static ProductEntity ToEntity(ProductDto dto)
+        {
+            dto.ProductID ??= 0;
+            dto.DateAdded ??= DateTime.UtcNow;
+
+            return new ProductEntity()
+            {
+                ProductID = dto.ProductID.Value,
+                CategoryID = dto.CategoryID,
+                ProductName = dto.ProductName,
+                ProductCode = dto.ProductCode,
+                ListPrice = dto.ListPrice,
+                DiscountPercent = dto.DiscountPercent,
+                Description = dto.Description,
+                DateAdded = dto.DateAdded
+            };
+        }
+
+        public static ProductDto ToDto(ProductEntity entity)
+        {
+            return new ProductDto()
+            {
+                ProductID = entity.ProductID,
+                CategoryID = entity.CategoryID,
+                ProductCode = entity.ProductCode,
+                ProductName = entity.ProductName,
+                Description = entity.Description,
+                ListPrice = entity.ListPrice,
+                DiscountPercent = entity.DiscountPercent,
+                DateAdded = entity.DateAdded
+            };
+        }
+    }
+}
