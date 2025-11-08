e89cfe89a967e4f11fc48e3e74313b7ddcc36600
Arthur Spears <arthurtspears@gmail.com>
2025-11-08 11:47:16 -0800

    First migration complete!

---
+++ MyGuitarShop.Data.EFCore/Factories/MyGuitarShopContextFactory.cs
@@ -0,0 +1,32 @@
+﻿
+
+
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using MyGuitarShop.Data.EFCore.Context;
+
+namespace MyGuitarShop.Data.EFCore.Factories
+{ 
+    public class MyGuitarShopContextFactory : IDesignTimeDbContextFactory<MyGuitarShopContext>
+    {
+        public MyGuitarShopContext CreateDbContext(string[] args)
+        {
+            // Build configuration
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Get connection string
+            var connectionString = configuration.GetConnectionString("MyGuitarShopMigrations");
+
+            // Build options
+            var optionsBuilder = new DbContextOptionsBuilder<MyGuitarShopContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            // Create and return context
+            return new MyGuitarShopContext(optionsBuilder.Options);
+        }
+    }
+}
